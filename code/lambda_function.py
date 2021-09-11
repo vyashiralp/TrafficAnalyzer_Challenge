035f86ce4cf049bc7f08f9f0db444dadd3dc6db8
vyashiralp <90488900+vyashiralp@users.noreply.github.com>
2021-09-10 22:03:39 -0500

    Update lambda_function.py

--- code/lambda_function.py
+++ code/lambda_function.py
@@ -1,5 +1,3 @@
-#import modin.pandas as pd
-#from modin.utils import to_pandas
 import pandas as pd
 import urllib.parse as urlparse 
 from urllib.parse import parse_qs
@@ -86,19 +84,21 @@ class TrafficAnalyser(object):
 
 
 	def generate_summary_report(self, df):
-		"""
+		""" 
+		Generates a dataframe with 3 columns referrer name, keyword and total_revenue 
 		"""
 		return df.groupby(['referrer_host', 'potential_search_term']).agg({'total_revenue':'sum'}).reset_index().sort_values(by= 'total_revenue',ascending=False)
 
 	def generate_long_report(self, df):
-		"""
+		""" 
+		Generates dataframe with extra columns if client needs to look at fields other than referrer name, keyword and total_revenue
 		"""
 		return df[[self.long_report_columns]]
 
 
 	def get_search_term(self,url):
 		"""
-		Try extracting search term from each URL
+		extracting search term from each URL
 
 		"""
 		search_term_keys = ('q','p','k')  #This list will have to evolve over time to improve accuracy of discovering keywords
@@ -128,6 +128,9 @@ class TrafficAnalyser(object):
 
 
 	def get_revenue(self,df):
+		"""
+		Getting revenue from rows which had event_list as 1 indicating a purchase
+		"""
 		output_columns = ['geo_city', 'geo_region', 'geo_country', 'category', 'product_name', 'number_of_items', 'total_revenue']
 		if 1 in df.event_list:
 			return df[df.event_list == 1][output_columns]
@@ -135,15 +138,16 @@ class TrafficAnalyser(object):
 			return "Event list is not 1"
 
 	def get_search_engine_keyword(self,df):
+		"""
+		Getting only those rows which does not have an NaN search keyword
+		"""
 		df.sort_values('date_time', inplace = True)
-
-		keywords = df['potential_search_term'].unique()
-		hosts = df['referrer_host'].unique()
 
 		return df[~df.potential_search_term.isna()][['potential_search_term', 'referrer_host']].iloc[0]
 
 	def process_file(self):
 		"""
+		main function which calls the preprocessing, extract and summary functions
 		"""
 		df = pd.read_csv("s3://website-traffic-artifacts/"+self.filepath,delimiter = '\t')
 		processed_df = self.preprocess_product_list(df)
@@ -160,7 +164,9 @@ class TrafficAnalyser(object):
 		return output_df
 
 def check_file_exist_s3(client, bucket, key):
-    """return the key's size if it exist, else None"""
+    """
+    return the key's size if it exist, else None
+    """
     prefix = "output"
     key = 'output/' + key
     response = client.list_objects_v2(
@@ -173,6 +179,11 @@ def check_file_exist_s3(client, bucket, key):
         	return obj['Size']
 			
 def send_email(exec_date,bucketname,filename,output_filename):
+	"""
+	Function checks calls the function to check if output file exists in the s3. If exists, sends sns notifications to the subscribers. 
+	If file exists but the size if <0 then returns "File Successfully created but with size 0" else returns url of the uploaded file.
+	
+	"""
 	s3_client = boto3.client("s3",region_name= 'us-west-2')
 	file_size = check_file_exist_s3(s3_client,bucketname,output_filename)
 	client = boto3.client('sns')
@@ -193,16 +204,18 @@ def send_email(exec_date,bucketname,filename,output_filename):
 		
 		
 def lambda_handler(event, context):
-
-	filename = event['Records'][0]['s3']['object']['key']
-	bucketname= event['Records'][0]['s3']['bucket']['name']
-	tf = TrafficAnalyser(filename)
-	result = tf.process_file()
-	exec_date= date.today()
+	"""
+	Handler function which calls class methods.
+	"""
+	filename = event['Records'][0]['s3']['object']['key'] # retrieves input file name uploaded in s3
+	bucketname= event['Records'][0]['s3']['bucket']['name'] # retrieves bucket name where file was uploaded
+	tf = TrafficAnalyser(filename) #creating instance of class TrafficAnalyser
+	result = tf.process_file() #calls initial function of the class to start the preprocess steps
+	exec_date= date.today()  #extracts today's date
 	result.columns = ['Search Engine Domain','Search Keywords','Revenue']
-	output_filename = str(exec_date)+'_SearchKeywordPerformance.tab.tsv'
-	result.to_csv("s3://"+bucketname+"/output/"+output_filename,sep ='\t', index=False)
-	send_email(exec_date,bucketname,filename,output_filename)
+	output_filename = str(exec_date)+'_SearchKeywordPerformance.tab.tsv'   #create output file name with current execution date
+	result.to_csv("s3://"+bucketname+"/output/"+output_filename,sep ='\t', index=False) #creates csv from output dataframe
+	send_email(exec_date,bucketname,filename,output_filename) #calls send_email function to send out respective sns notifications
 	
 		
 	
